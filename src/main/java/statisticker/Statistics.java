dd55eac28d98419fb158c889391438af51d10263
varunbh79 <46313795+varunbh79@users.noreply.github.com>
2022-05-05 15:49:01 +0530

    Created Stats Class for return

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
+    class Stats {
+        
+    }    
+    
+    public static Stats getStatistics(List<Float> numbers) {
         //implement the computation of statistics here
     }
 }
